81d7f4dda0b00fcef27f57fa583b278ab1feede0
Danis Alfonso <danis.ramirez.hn@gmail.com>
2023-09-03 22:56:06 +0200

    Beispiele

---
+++ examples/LinearAlgebra.cpp
@@ -0,0 +1,10 @@
+#include "math/utils.hpp"
+#include "math/LinearAlgebra/LUDecomposition.hpp"
+
+int main() {
+    const int n = 3;
+    nm::MatrixDouble a(n, n);
+    nm::VectorDouble b(n);
+
+    std::cout << "LUDecomposition" << std::endl;
+}
